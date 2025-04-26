172e5161ac3d264dacc1db273a9fc431f18ecda8
lmortiz6 <loganmortiz@gmail.com>
2025-04-26 15:29:56 -0400

    Add Strike

---
+++ src/main/java/theartifex/cards/attacks/Strike_Artifex.java
@@ -0,0 +1,45 @@
+package theartifex.cards.attacks;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theartifex.cards.BaseCard;
+import theartifex.character.TheArtifexCharacter;
+import theartifex.util.CardStats;
+
+public class Strike_Artifex extends BaseCard {
+
+    public static final String ID = makeID(Strike_Artifex.class.getSimpleName());
+
+    private static final CardStats info = new CardStats(
+            TheArtifexCharacter.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
+            CardType.ATTACK,
+            CardRarity.BASIC,
+            CardTarget.ENEMY,
+            1
+    );
+    private static final int DAMAGE = 6;
+    private static final int UPG_DAMAGE = 3;
+
+    public Strike_Artifex() {
+        super(ID, info); //Pass the required information to the BaseCard constructor.
+
+        setDamage(DAMAGE, UPG_DAMAGE); //Sets the card's damage and how much it changes when upgraded.
+
+        tags.add(CardTags.STARTER_STRIKE);
+        tags.add(CardTags.STRIKE);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new Strike_Artifex();
+    }
+}
