ad86b732bd0dc3331db114c574ea42ea36e01c83
lmortiz6 <loganmortiz@gmail.com>
2025-04-26 15:38:30 -0400

    Clean Up Imports

--- src/main/java/theartifex/cards/skills/Defend_Artifex.java
+++ src/main/java/theartifex/cards/skills/Defend_Artifex.java
@@ -1,10 +1,7 @@
 package theartifex.cards.skills;
 
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import theartifex.cards.BaseCard;
