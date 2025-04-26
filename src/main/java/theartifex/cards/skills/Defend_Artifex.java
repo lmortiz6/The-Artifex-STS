7981e1b8c7d586f9029de0e9c8c32869334f0851
lmortiz6 <loganmortiz@gmail.com>
2025-04-26 15:37:30 -0400

    Add Defend

---
+++ src/main/java/theartifex/cards/skills/Defend_Artifex.java
@@ -0,0 +1,45 @@
+package theartifex.cards.skills;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theartifex.cards.BaseCard;
+import theartifex.character.TheArtifexCharacter;
+import theartifex.util.CardStats;
+
+public class Defend_Artifex extends BaseCard {
+
+    public static final String ID = makeID(Defend_Artifex.class.getSimpleName());
+
+    private static final CardStats info = new CardStats(
+            TheArtifexCharacter.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
+            CardType.SKILL,
+            CardRarity.BASIC,
+            CardTarget.SELF,
+            1
+    );
+    private static final int BLOCK = 5;
+    private static final int UPG_BLOCK = 3;
+
+    public Defend_Artifex() {
+        super(ID, info); //Pass the required information to the BaseCard constructor.
+
+        setBlock(BLOCK, UPG_BLOCK); //Sets the card's damage and how much it changes when upgraded.
+
+        tags.add(CardTags.STARTER_DEFEND);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new GainBlockAction(p, p, this.block));
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new Defend_Artifex();
+    }
+}
