6df4d4eff26584aedab15e5a11bcd6b5ed891693
lmortiz6 <loganmortiz@gmail.com>
2025-04-26 17:28:26 -0400

    Add Cleave

---
+++ src/main/java/theartifex/cards/attacks/CleaveArmor.java
@@ -0,0 +1,48 @@
+package theartifex.cards.attacks;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import theartifex.cards.BaseCard;
+import theartifex.character.TheArtifexCharacter;
+import theartifex.util.CardStats;
+
+public class CleaveArmor extends BaseCard {
+
+    public static final String ID = makeID(CleaveArmor.class.getSimpleName());
+
+    private static final CardStats info = new CardStats(
+            TheArtifexCharacter.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
+            CardType.ATTACK,
+            CardRarity.COMMON,
+            CardTarget.ENEMY,
+            1
+    );
+    private static final int DAMAGE = 5;
+    private static final int UPG_DAMAGE = 3;
+    private static final int VULN = 1;
+    private static final int UPG_VULN = 1;
+
+    public CleaveArmor() {
+        super(ID, info); //Pass the required information to the BaseCard constructor.
+
+        setDamage(DAMAGE, UPG_DAMAGE); //Sets the card's damage and how much it changes when upgraded.
+        setMagic(VULN, UPG_VULN);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_HEAVY));
+        addToBot(new ApplyPowerAction(m, p, new VulnerablePower(m, this.magicNumber, false), this.magicNumber));
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new CleaveArmor();
+    }
+}
