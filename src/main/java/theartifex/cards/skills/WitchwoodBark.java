803e6a64655af7581c9b4dcde16e50f6ce96c360
lmortiz6 <loganmortiz@gmail.com>
2025-04-26 16:25:29 -0400

    Add Witchwood Bark

---
+++ src/main/java/theartifex/cards/skills/WitchwoodBark.java
@@ -0,0 +1,51 @@
+package theartifex.cards.skills;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.RegenPower;
+import theartifex.cards.BaseCard;
+import theartifex.character.TheArtifexCharacter;
+import theartifex.util.CardStats;
+
+public class WitchwoodBark extends BaseCard {
+
+    public static final String ID = makeID(WitchwoodBark.class.getSimpleName());
+
+    private static final CardStats info = new CardStats(
+            TheArtifexCharacter.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
+            CardType.SKILL,
+            CardRarity.BASIC,
+            CardTarget.SELF,
+            1
+    );
+    private static final int REGEN = 3;
+
+    public WitchwoodBark() {
+        super(ID, info); //Pass the required information to the BaseCard constructor.
+
+        this.baseMagicNumber = REGEN;
+        this.magicNumber = baseMagicNumber;
+        this.setExhaust(true);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new RegenPower(p, magicNumber)));
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            upgradeName();
+            upgradeBaseCost(0);
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new WitchwoodBark();
+    }
+}
