8dc7bcff6f5f874240d0e33e16f707d19c1fc6a9
Logan <loganmortiz@gmail.com>
2026-01-11 13:29:41 -0500

    Change Eaters Crest to shop relic

--- src/main/java/theartifex/relics/EatersCrestRelic.java
+++ src/main/java/theartifex/relics/EatersCrestRelic.java
@@ -1,12 +1,13 @@
 package theartifex.relics;
 
+import basemod.devcommands.deck.DeckAdd;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.helpers.RelicLibrary;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
-import com.megacrit.cardcrawl.rewards.RewardItem;
 import theartifex.TheArtifexMod;
 import theartifex.abstracts.AbstractCreditRelic;
-import theartifex.abstracts.AbstractCyberneticRelic;
+import theartifex.abstracts.AbstractCyberneticCard;
 
 import java.util.ArrayList;
 
@@ -16,12 +17,11 @@ public class EatersCrestRelic extends AbstractCreditRelic {
 
     private static final String NAME = EatersCrestRelic.class.getSimpleName();
     public static final String ID = makeID(NAME);
-    private static final AbstractRelic.RelicTier RARITY = RelicTier.BOSS;
+    private static final AbstractRelic.RelicTier RARITY = RelicTier.SHOP;
     private static final AbstractRelic.LandingSound SOUND = LandingSound.MAGICAL;
     private static final int AMOUNT = 3;
 
-    private boolean active = false;
-    private final ArrayList<AbstractRelic> possibleRelics = new ArrayList<>();
+    private final ArrayList<AbstractCard> possibleCards = new ArrayList<>();
 
     public EatersCrestRelic() {
         super(ID, NAME, RARITY, SOUND, AMOUNT);
@@ -33,31 +33,12 @@ public class EatersCrestRelic extends AbstractCreditRelic {
 
     @Override
     public void onEquip() {
-        active = true;
-
-        for (AbstractRelic r : RelicLibrary.specialList) {
-            if (r instanceof AbstractCyberneticRelic && ((AbstractCyberneticRelic)r).getCost() >= 3 && ((AbstractCyberneticRelic)r).getCost() <= TheArtifexMod.getAvailableCredits()) {
-                possibleRelics.add(r);
+        for (AbstractCard c : CardLibrary.getAllCards()) {
+            if (c instanceof AbstractCyberneticCard && c.rarity == AbstractCard.CardRarity.RARE && c.cost <= TheArtifexMod.getAvailableCredits()) {
+                possibleCards.add(c);
             }
         }
-    }
-
-    @Override
-    public void update() {
-        super.update();
-        if (active && !AbstractDungeon.isScreenUp) {
-            if (!possibleRelics.isEmpty()) {
-                TheArtifexMod.logger.info(possibleRelics.toString());
-                int i = AbstractDungeon.relicRng.random(possibleRelics.size() - 1);
-                AbstractDungeon.combatRewardScreen.open();
-                AbstractDungeon.combatRewardScreen.rewards.clear();
-                AbstractDungeon.combatRewardScreen.rewards.add(new RewardItem(RelicLibrary.getRelic(possibleRelics.get(i).relicId).makeCopy()));
-                AbstractDungeon.combatRewardScreen.positionRewards();
-                AbstractDungeon.overlayMenu.proceedButton.setLabel("Skip Reward");
-                active = false;
-                (AbstractDungeon.getCurrRoom()).rewardPopOutTimer = 0.25F;
-            }
-            active = false;
-        }
+        int i = AbstractDungeon.relicRng.random(possibleCards.size() - 1);
+        DeckAdd.execute(new String[] {"deck", "add", possibleCards.get(i).cardID});
     }
 }
