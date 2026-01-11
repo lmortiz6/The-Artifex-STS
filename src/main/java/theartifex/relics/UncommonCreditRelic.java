c7c141bdcde2bfda252791390acee70427208daa
Logan <loganmortiz@gmail.com>
2026-01-11 13:04:50 -0500

    Change credit relic drops

--- src/main/java/theartifex/relics/UncommonCreditRelic.java
+++ src/main/java/theartifex/relics/UncommonCreditRelic.java
@@ -8,7 +8,7 @@ import static theartifex.TheArtifexMod.makeID;
 public class UncommonCreditRelic extends AbstractCreditRelic {
     private static final String NAME = UncommonCreditRelic.class.getSimpleName();
     public static final String ID = makeID(NAME);
-    private static final AbstractRelic.RelicTier RARITY = RelicTier.UNCOMMON;
+    private static final AbstractRelic.RelicTier RARITY = RelicTier.SPECIAL;
     private static final AbstractRelic.LandingSound SOUND = LandingSound.FLAT;
     private static final int AMOUNT = 3;
 
